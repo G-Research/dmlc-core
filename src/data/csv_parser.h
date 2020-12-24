dc6644ac4c374f002e87f0eb3fa60f6afa4647b1
TP Boudreau <tpboudreau@gmail.com>
2020-12-24 18:21:18 +0000

    add GR features

--- src/data/csv_parser.h
+++ src/data/csv_parser.h
@@ -23,22 +23,128 @@ namespace data {
 
 struct CSVParserParam : public Parameter<CSVParserParam> {
   std::string format;
-  int label_column;
+  std::string label_column;
   std::string delimiter;
   int weight_column;
+  int subsample_group_column;
+  int subsample_mask_column;
+  std::string feature_columns;
   // declare parameters
   DMLC_DECLARE_PARAMETER(CSVParserParam) {
     DMLC_DECLARE_FIELD(format).set_default("csv")
         .describe("File format.");
-    DMLC_DECLARE_FIELD(label_column).set_default(-1)
-        .describe("Column index (0-based) that will put into label.");
+    DMLC_DECLARE_FIELD(label_column).set_default("")
+        .describe("List of column indices that represent labels.");
     DMLC_DECLARE_FIELD(delimiter).set_default(",")
-      .describe("Delimiter used in the csv file.");
+        .describe("Delimiter used in the csv file.");
     DMLC_DECLARE_FIELD(weight_column).set_default(-1)
         .describe("Column index that will put into instance weights.");
+    DMLC_DECLARE_FIELD(subsample_group_column).set_default(-1)
+        .describe("Column index of the group number (for grouped subsampling) for the instance.");
+    DMLC_DECLARE_FIELD(subsample_mask_column).set_default(-1)
+        .describe("Column index of the predicate value (for masked subsampling) for the instance.");
+    DMLC_DECLARE_FIELD(feature_columns).set_default("")
+      .describe("Subset of feature column indices that should be retained.");
+  }
+
+  static const char kLabelColumnListDelimiter = ',';
+  std::map<int, int> label_column_indices;
+  size_t label_count;
+
+  void ExtractLabelColumnIndices() {
+    if (label_column.length() > 0) {
+      std::string element;
+      std::stringstream list(label_column);
+      int output_index = 0;
+      while (std::getline(list, element, kLabelColumnListDelimiter)) {
+        if (!element.empty()) {
+          const char front = element.front();
+          if (isdigit(front) || front == '-' || front == '+') {
+            std::string::size_type next = 0;
+            int input_index = std::stoi(element, &next);
+            if (next == element.size()) {
+              if (input_index >= 0) {
+                const auto inserted = label_column_indices.insert({input_index, output_index});
+                if (inserted.second) { // insert succeeded
+                  output_index++;
+                } else {
+                  LOG(WARNING) << "Ignoring duplicate label_column index "
+                               << input_index;
+                }
+              } else {
+                LOG(WARNING) << "Ignoring negative label_column index "
+                             << input_index;
+              }
+            } else {
+              LOG(WARNING) << "Ignoring label_column list entry '"
+                           << element << "' "
+                           << "containing unexpected character '"
+                           << element.at(next) << "'";
+            }
+          } else {
+            LOG(WARNING) << "Ignoring non-numeric label_column list entry '"
+                         << element << "'";
+          }
+        } else {
+          LOG(WARNING) << "Ignoring missing label_column list entry";
+        }
+      }
+    }
+    label_count = (label_column_indices.size() > 1 ? label_column_indices.size() : 1);
+    return;
+  }
+
+  static const char kFeatureColumnListDelimiter = ',';
+  std::map<int, int> feature_column_indices;
+
+  void ExtractFeatureColumnIndices() {
+    if (feature_columns.length() > 0) {
+      std::string element;
+      std::stringstream list(feature_columns);
+      int output_index = 0;
+      while (std::getline(list, element, kFeatureColumnListDelimiter)) {
+        if (!element.empty()) {
+          const char front = element.front();
+          if (isdigit(front) || front == '-' || front == '+') {
+            std::string::size_type next = 0;
+            int input_index = std::stoi(element, &next);
+            if (next == element.size()) {
+              if (input_index >= 0) {
+                const auto inserted = feature_column_indices.insert({input_index, output_index});
+                if (inserted.second) { // insert succeeded
+                  output_index++;
+                } else {
+                  LOG(WARNING) << "Ignoring duplicate feature_column index "
+                               << input_index;
+                }
+              } else {
+                LOG(WARNING) << "Ignoring negative feature_column index "
+                             << input_index;
+              }
+            } else {
+              LOG(WARNING) << "Ignoring feature_column list entry '"
+                           << element << "' "
+                           << "containing unexpected character '"
+                           << element.at(next) << "'";
+            }
+          } else {
+            LOG(WARNING) << "Ignoring non-numeric feature_column list entry '"
+                         << element << "'";
+          }
+        } else {
+          LOG(WARNING) << "Ignoring missing feature_column list entry";
+        }
+      }
+      for (const auto& label_column : label_column_indices) {
+        feature_column_indices.erase(label_column.first);
+      }
+      feature_column_indices.erase(weight_column);
+      feature_column_indices.erase(subsample_group_column);
+      feature_column_indices.erase(subsample_mask_column);
+    }
+    return;
   }
 };
-
 
 /*!
  * \brief CSVParser, parses a dense csv format.
@@ -56,9 +162,11 @@ class CSVParser : public TextParserBase<IndexType, DType> {
                      int nthread)
       : TextParserBase<IndexType, DType>(source, nthread) {
     param_.Init(args);
+    param_.ExtractLabelColumnIndices();
+    param_.ExtractFeatureColumnIndices();
     CHECK_EQ(param_.format, "csv");
-    CHECK(param_.label_column != param_.weight_column
-          || param_.label_column < 0)
+    CHECK(param_.label_column_indices.find(param_.weight_column) ==
+          param_.label_column_indices.end())
       << "Must have distinct columns for labels and instance weights";
   }
 
@@ -77,6 +185,8 @@ ParseBlock(const char *begin,
            const char *end,
            RowBlockContainer<IndexType, DType> *out) {
   out->Clear();
+  out->label_count = param_.label_count;
+  std::vector<DType> label(param_.label_count);
   const char * lbegin = begin;
   const char * lend = lbegin;
   // advance lbegin if it points to newlines
@@ -90,37 +200,57 @@ ParseBlock(const char *begin,
     const char* p = lbegin;
     int column_index = 0;
     IndexType idx = 0;
-    DType label = DType(0.0f);
+    std::fill(label.begin(), label.end(), DType(0.0f));
     real_t weight = std::numeric_limits<real_t>::quiet_NaN();
+    int64_t subsample_group = -1;
+    int64_t subsample_mask = -1;
 
     while (p != lend) {
       char *endptr;
       DType v;
-      // if DType is float32
-      if (std::is_same<DType, real_t>::value) {
-        v = strtof(p, &endptr);
-      // If DType is int32
-      } else if (std::is_same<DType, int32_t>::value) {
-        v = static_cast<int32_t>(strtoll(p, &endptr, 0));
-      // If DType is int64
-      } else if (std::is_same<DType, int64_t>::value) {
-        v = static_cast<int64_t>(strtoll(p, &endptr, 0));
-      // If DType is all other types
-      } else {
-        LOG(FATAL) << "Only float32, int32, and int64 are supported for the time being";
+      if (column_index != param_.subsample_group_column &&
+          column_index != param_.subsample_mask_column) {
+        // if DType is float32
+        if (std::is_same<DType, real_t>::value) {
+          v = strtof(p, &endptr);
+        // If DType is int32
+        } else if (std::is_same<DType, int32_t>::value) {
+          v = static_cast<int32_t>(strtoll(p, &endptr, 0));
+        // If DType is int64
+        } else if (std::is_same<DType, int64_t>::value) {
+          v = static_cast<int64_t>(strtoll(p, &endptr, 0));
+        // If DType is all other types
+        } else {
+          LOG(FATAL) << "Only float32, int32, and int64 are supported for the time being";
+        }
       }
 
-      if (column_index == param_.label_column) {
-        label = v;
+      const auto label_column_found = param_.label_column_indices.find(column_index);
+      if (label_column_found != param_.label_column_indices.end()) {
+        label[label_column_found->second] = v;
       } else if (std::is_same<DType, real_t>::value
                  && column_index == param_.weight_column) {
         weight = v;
+      } else if (column_index == param_.subsample_group_column) {
+        subsample_group = static_cast<int64_t>(strtoll(p, &endptr, 0));
+      } else if (column_index == param_.subsample_mask_column) {
+        subsample_mask = static_cast<int64_t>(strtoll(p, &endptr, 0));
       } else {
-        if (std::distance(p, static_cast<char const*>(endptr)) != 0) {
-          out->value.push_back(v);
-          out->index.push_back(idx++);
+        if (param_.feature_column_indices.empty()) {
+          if (std::distance(p, static_cast<char const*>(endptr)) != 0) {
+            out->value.push_back(v);
+            out->index.push_back(idx);
+          }
+          ++idx;
         } else {
-          idx++;
+          const auto found = param_.feature_column_indices.find(column_index);
+          if (found != param_.feature_column_indices.end()) {
+            if (std::distance(p, static_cast<char const*>(endptr)) != 0) {
+              out->value.push_back(v);
+              out->index.push_back(found->second);
+            }
+            ++idx;
+          }
         }
       }
       p = (endptr >= lend) ? lend : endptr;
@@ -136,14 +266,25 @@ ParseBlock(const char *begin,
     // skip empty line
     while ((*lend == '\n' || *lend == '\r') && lend != end) ++lend;
     lbegin = lend;
-    out->label.push_back(label);
+
+    out->label.insert(out->label.cend(), label.cbegin(), label.cend());
     if (!std::isnan(weight)) {
       out->weight.push_back(weight);
     }
+    if (subsample_group >= 0 && subsample_group <= std::numeric_limits<uint32_t>::max()) {
+      out->subsample_group.push_back(static_cast<uint32_t>(subsample_group));
+    }
+    if (subsample_mask >= 0 && subsample_mask <= std::numeric_limits<uint32_t>::max()) {
+      out->subsample_mask.push_back(static_cast<uint32_t>(subsample_mask));
+    }
     out->offset.push_back(out->index.size());
   }
-  CHECK(out->label.size() + 1 == out->offset.size());
+  CHECK_GT(out->label_count, 0);
+  CHECK_EQ(out->label.size() % out->label_count, 0);
+  CHECK_EQ((out->label.size() / out->label_count) + 1, out->offset.size());
   CHECK(out->weight.size() == 0 || out->weight.size() + 1 == out->offset.size());
+  CHECK(out->subsample_group.size() == 0 || out->subsample_group.size() + 1 == out->offset.size());
+  CHECK(out->subsample_mask.size() == 0 || out->subsample_mask.size() + 1 == out->offset.size());
 }
 }  // namespace data
 }  // namespace dmlc
--- src/data/row_block.h
+++ src/data/row_block.h
@@ -27,10 +27,16 @@ template<typename IndexType, typename DType = real_t>
 struct RowBlockContainer {
   /*! \brief array[size+1], row pointer to beginning of each rows */
   std::vector<size_t> offset;
-  /*! \brief array[size] label of each instance */
+  /*! \brief number of labels for the instance */
+  size_t label_count;
+  /*! \brief array[size * label_count] label(s) of each instance */
   std::vector<DType> label;
   /*! \brief array[size] weight of each instance */
   std::vector<real_t> weight;
+  /*! \brief array[size] subsample group number of each instance */
+  std::vector<uint32_t> subsample_group;
+  /*! \brief array[size] subsample predicate value of each instance */
+  std::vector<uint32_t> subsample_mask;
   /*! \brief array[size] session-id of each instance */
   std::vector<uint64_t> qid;
   /*! \brief field index */
@@ -63,7 +69,15 @@ struct RowBlockContainer {
   /*! \brief clear the container */
   inline void Clear(void) {
     offset.clear(); offset.push_back(0);
-    label.clear(); field.clear(); index.clear(); value.clear(); weight.clear(); qid.clear();
+    label.clear();
+    label_count = 0;
+    field.clear();
+    index.clear();
+    value.clear();
+    weight.clear();
+    subsample_group.clear();
+    subsample_mask.clear();
+    qid.clear();
     max_field = 0;
     max_index = 0;
   }
@@ -74,9 +88,11 @@ struct RowBlockContainer {
   /*! \return estimation of memory cost of this container */
   inline size_t MemCostBytes(void) const {
     return offset.size() * sizeof(size_t) +
-        label.size() * sizeof(real_t) +
+        label.size() * sizeof(DType) +
         weight.size() * sizeof(real_t) +
-        qid.size() * sizeof(size_t) +
+        subsample_group.size() * sizeof(uint32_t) +
+        subsample_mask.size() * sizeof(uint32_t) +
+        qid.size() * sizeof(uint64_t) +
         field.size() * sizeof(IndexType) +
         index.size() * sizeof(IndexType) +
         value.size() * sizeof(DType);
@@ -88,8 +104,21 @@ struct RowBlockContainer {
    */
   template<typename I>
   inline void Push(Row<I, DType> row) {
-    label.push_back(row.get_label());
+    // TODO(tpb) -- test copy of multiple labels
+    if (label_count == 0) {
+      label_count = row.get_label_count();
+    }
+    CHECK_EQ(label_count, row->get_label_count())
+        << "each row must contain the same number of labels";
+    if (label_count == 1) {
+      label.push_back(row.get_label());
+    } else {
+      auto lb = row.get_all_labels();
+      label.insert(label.cend(), lb.cbegin(), lb.cend());
+    }
     weight.push_back(row.get_weight());
+    subsample_group.push_back(row.get_subsample_group());
+    subsample_mask.push_back(row.get_subsample_mask());
     qid.push_back(row.get_qid());
     if (row.field != NULL) {
       for (size_t i = 0; i < row.length; ++i) {
@@ -121,12 +150,31 @@ struct RowBlockContainer {
    */
   template<typename I>
   inline void Push(RowBlock<I, DType> batch) {
-    size_t size = label.size();
-    label.resize(label.size() + batch.size);
-    std::memcpy(BeginPtr(label) + size, batch.label,
-                batch.size * sizeof(DType));
+    // TODO(tpb) test copy of multiple labels for multiple rows
+    if (label_count == 0) {
+      label_count = batch.label_count;
+    }
+    CHECK_EQ(label_count, batch.label_count)
+        << "each row block must contain the same number of labels";
+    size_t label_size = label.size();
+    size_t size = label_size / label_count;
+    size_t batch_label_size = batch.size * batch.label_count;
+    label.resize(label_size + batch_label_size);
+    std::memcpy(BeginPtr(label) + label_size,
+                batch.label, batch_label_size * sizeof(DType));
+
     if (batch.weight != NULL) {
       weight.insert(weight.end(), batch.weight, batch.weight + batch.size);
+    }
+    if (batch.subsample_group != NULL) {
+      subsample_group.insert(subsample_group.end(),
+                             batch.subsample_group,
+                             batch.subsample_group + batch.size);
+    }
+    if (batch.subsample_mask != NULL) {
+      subsample_mask.insert(subsample_mask.end(),
+                            batch.subsample_mask,
+                            batch.subsample_mask + batch.size);
     }
     if (batch.qid != NULL) {
       qid.insert(qid.end(), batch.qid, batch.qid + batch.size);
@@ -171,15 +219,18 @@ inline RowBlock<IndexType, DType>
 RowBlockContainer<IndexType, DType>::GetBlock(void) const {
   // consistency check
   if (label.size()) {
-    CHECK_EQ(label.size() + 1, offset.size());
+    CHECK_EQ(label.size() + label_count, offset.size() * label_count);
   }
   CHECK_EQ(offset.back(), index.size());
   CHECK(offset.back() == value.size() || value.size() == 0);
   RowBlock<IndexType, DType> data;
   data.size = offset.size() - 1;
   data.offset = BeginPtr(offset);
+  data.label_count = label_count;
   data.label = BeginPtr(label);
   data.weight = BeginPtr(weight);
+  data.subsample_group = BeginPtr(subsample_group);
+  data.subsample_mask = BeginPtr(subsample_mask);
   data.qid = BeginPtr(qid);
   data.field = BeginPtr(field);
   data.index = BeginPtr(index);
@@ -190,8 +241,11 @@ template<typename IndexType, typename DType>
 inline void
 RowBlockContainer<IndexType, DType>::Save(Stream *fo) const {
   fo->Write(offset);
+  fo->Write(&label_count, sizeof(size_t));
   fo->Write(label);
   fo->Write(weight);
+  fo->Write(subsample_group);
+  fo->Write(subsample_mask);
   fo->Write(qid);
   fo->Write(field);
   fo->Write(index);
@@ -203,8 +257,11 @@ template<typename IndexType, typename DType>
 inline bool
 RowBlockContainer<IndexType, DType>::Load(Stream *fi) {
   if (!fi->Read(&offset)) return false;
+  CHECK(fi->Read(&label_count, sizeof(size_t))) << "Bad RowBlock format";
   CHECK(fi->Read(&label)) << "Bad RowBlock format";
   CHECK(fi->Read(&weight)) << "Bad RowBlock format";
+  CHECK(fi->Read(&subsample_group)) << "Bad RowBlock format";
+  CHECK(fi->Read(&subsample_mask)) << "Bad RowBlock format";
   CHECK(fi->Read(&qid)) << "Bad RowBlock format";
   CHECK(fi->Read(&field)) << "Bad RowBlock format";
   CHECK(fi->Read(&index)) << "Bad RowBlock format";
